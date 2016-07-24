a8c501372d04ece664920d519d3d8671e0463d1e
immanoj <manojkchakravarthy@gmail.com>
2016-07-24 19:53:21 +0530

    First Change To Commit

--- SampleMVC1/Controllers/HomeController.cs
+++ SampleMVC1/Controllers/HomeController.cs
@@ -10,7 +10,8 @@ namespace SampleMVC1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            //ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "First Change to commit";
 
             return View();
         }
